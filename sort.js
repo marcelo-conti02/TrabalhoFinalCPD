9bcfa920ed550fc12d3f9b2a8fca34de8c05b94d
Marcelo Conti <marceloconti9017@gmail.com>
2024-02-05 20:05:09 -0300

    InsertionSort com busca binária em sort.js

--- sort.js
+++ sort.js
@@ -3,8 +3,34 @@ const fs = require('fs');
 const jsonString = fs.readFileSync('places.json', 'utf-8');
 const jsonData = JSON.parse(jsonString);
 
-// exclui os objetos com name vazio e ordena por ordem alfabetica
-jsonData.all = jsonData.all.filter(obj => obj.name.trim() !== '').sort((a, b) => a.name.localeCompare(b.name));
+function binarySearch(array, key, start, end) {
+    while (start <= end) {
+        const mid = Math.floor((start + end) / 2);
+        if (array[mid].name < key) {
+            start = mid + 1;
+        } else if (array[mid].name > key) {
+            end = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return start;
+}
 
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        const key = array[i];
+        const index = binarySearch(array, key.name, 0, i - 1);
+        for (let j = i; j > index; j--) {
+            array[j] = array[j - 1];
+        }
+        array[index] = key;
+    }
+}
 
+// Filtra objetos com 'name' vazio e ordena
+jsonData.all = jsonData.all.filter(obj => obj.name.trim() !== '');
+insertionSort(jsonData.all);
+
+// Reescreve o arquivo ordenado
 fs.writeFileSync('places.json', JSON.stringify(jsonData, null, 2));

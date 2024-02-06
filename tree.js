95e4d2aef323040e62e91a5f09e53b0df17880ba
VIsatto <140186601+VIsatto@users.noreply.github.com>
2024-02-06 14:38:57 -0300

    HashTree.js

--- tree.js
+++ tree.js
@@ -1,171 +1,83 @@
-class BPlusTree {
-  constructor(order) {
-    this.root = null;
-    this.order = order;
+class Hashing {
+  constructor(tam) {
+    this.chart = new Array(tam);
+    this.size = 0;
   }
 
-  insert(key, value) {
-    if (!this.root) {
-      this.root = new BPlusNode(true);
-      this.root.keys.push({ key, value });
-      return;
-    }
 
-    this.root = this.root.insert(key, value);
-
-    // Split root if necessary
-    if (this.root.isOverflow()) {
-      const newRoot = new BPlusNode(false);
-      const split = this.root.split();
-      newRoot.children.push(this.root, split.newNode);
-      newRoot.keys.push(split.middleKey);
-      this.root = newRoot;
-    }
+  hash1(value) {
+    return (value % 19);
   }
 
-  search(key) {
-    return this.root ? this.root.search(key) : null;
+  hash2(value) {
+    return (value % 7) + 1;
   }
 
-  remove(key) {
-    if (this.root) {
-      this.root = this.root.remove(key);
-
-      // Adjust root if it has only one child
-      if (this.root.children.length === 1 && !this.root.isLeaf()) {
-        this.root = this.root.children[0];
+  get(key, remove = 0) {
+    let index = key
+    let flag = 0, cont = 0
+    let aux
+    index = this.hash1(index)
+    while (flag == 0) {
+      if (index >= this.tam) {
+        index = this.hash1(index)
+        cont = 0
       }
-    }
-  }
-}
 
-class BPlusNode {
-  constructor(isLeaf) {
-    this.isLeaf = isLeaf;
-    this.keys = [];
-    this.children = [];
-  }
-
-  insert(key, value) {
-    let index = 0;
-
-    while (index < this.keys.length && key > this.keys[index].key) {
-      index++;
-    }
-
-    if (this.isLeaf) {
-      this.keys.splice(index, 0, { key, value });
-    } else {
-      const child = this.children[index];
-      child.insert(key, value);
-
-      // Split child if necessary
-      if (child.isOverflow()) {
-        const split = child.split();
-        this.children.splice(index, 1, split.newNode);
-        this.keys.splice(index, 0, split.middleKey);
-      }
-    }
-
-    return this;
-  }
-
-  search(key) {
-    if (this.isLeaf) {
-      const keyIndex = this.keys.findIndex((k) => k.key === key);
-      return keyIndex !== -1 ? this.keys[keyIndex].value : null;
-    }
-
-    let index = 0;
-    while (index < this.keys.length && key > this.keys[index].key) {
-      index++;
-    }
-
-    return this.children[index].search(key);
-  }
-
-  remove(key) {
-    let index = 0;
-    while (index < this.keys.length && key > this.keys[index].key) {
-      index++;
-    }
-
-    if (this.isLeaf) {
-      if (this.keys[index] && this.keys[index].key === key) {
-        this.keys.splice(index, 1);
-      }
-    } else {
-      const child = this.children[index];
-      child.remove(key);
-
-      // Merge or redistribute children if necessary
-      if (child.isUnderflow()) {
-        const leftSibling = index > 0 ? this.children[index - 1] : null;
-        const rightSibling = index < this.children.length - 1 ? this.children[index + 1] : null;
-
-        if (rightSibling && rightSibling.keys.length > Math.floor(BPlusTree.order / 2)) {
-          const transferKey = rightSibling.keys.shift();
-          const transferChild = rightSibling.children.shift();
-          child.keys.push(this.keys[index]);
-          this.keys[index] = transferKey;
-          child.children.push(transferChild);
-        } else if (leftSibling && leftSibling.keys.length > Math.floor(BPlusTree.order / 2)) {
-          const transferKey = leftSibling.keys.pop();
-          const transferChild = leftSibling.children.pop();
-          child.keys.unshift(this.keys[index - 1]);
-          this.keys[index - 1] = transferKey;
-          child.children.unshift(transferChild);
-        } else if (rightSibling) {
-          // Merge with right sibling
-          const mergedKeys = child.keys.concat([this.keys[index]], rightSibling.keys);
-          const mergedChildren = child.children.concat(rightSibling.children);
-          this.keys.splice(index, 1);
-          this.children.splice(index, 2, new BPlusNode(false, mergedKeys, mergedChildren));
-        } else if (leftSibling) {
-          // Merge with left sibling
-          const mergedKeys = leftSibling.keys.concat([this.keys[index - 1]], child.keys);
-          const mergedChildren = leftSibling.children.concat(child.children);
-          this.keys.splice(index - 1, 1);
-          this.children.splice(index - 1, 2, new BPlusNode(false, mergedKeys, mergedChildren));
+      else {
+        if (this.chart[index].id == key)
+          flag = 1
+        else {
+          cont += 1
+          index = cont * this.hash2(index)
         }
       }
     }
+    aux = this.chart[index];
+    if (remove == 1) {
+      delete (this.chart[index])
+      this.size -= 1;
+    }
 
-    return this;
+    return aux
   }
 
-  split() {
-    const middleIndex = Math.floor(this.keys.length / 2);
-    const middleKey = this.keys[middleIndex];
-    const newNode = new BPlusNode(this.isLeaf);
+  set(obj) {
+    let key = obj.id;
+    let flag = 0, cont = 0;
+    key = this.hash1(key)
+    while (flag == 0) {
+      if (key >= this.tam) {
+        key = this.hash1(key)
+        cont = 0
+      }
 
-    newNode.keys = this.keys.splice(middleIndex + 1);
-    newNode.children = this.children.splice(middleIndex + 1);
+      else {
+        if (this.chart[key] == null) {
+          flag = 1
+        }
+        else {
+          cont += 1;
+          key += cont * this.hash2(key)
+        }
+      }
+    }
+    this.chart[key] = obj
+    this.size += 1
 
-    return { middleKey, newNode };
-  }
-
-  isOverflow() {
-    return this.keys.length > BPlusTree.order - 1;
-  }
-
-  isUnderflow() {
-    return this.keys.length < Math.ceil(BPlusTree.order / 2) - 1;
   }
 }
 
-// Usage example
-const bPlusTree = new BPlusTree(3);
 
-// Insert
-bPlusTree.insert(1, "One");
-bPlusTree.insert(2, "Two");
-bPlusTree.insert(3, "Gremio");
-bPlusTree.insert(4, "Four");
-bPlusTree.insert(5, "Five");
-bPlusTree.insert(6, "Six");
-bPlusTree.insert(7, "Seven");
 
-// Search
-let log=bPlusTree.search(3); // Output: Three
-console.log(log)
+// let tabela = new Hashing(50);
+
+// let all = [
+
+//   { "id": 1, "name": "Saúde no Copo Bom Fim", "type": "cafe" },
+//   { "id": 20, "name": "Cafeteria Cheirin Bão Bom", "type": "cafe" },
+// ]
+// let i = 0
+
+// for (i in all)
+//   tabela.set(all[i])

734a142c4fc540c31b4b4f44d4d56c235f45368c
Andre-klarmann <112667579+Andre-klarmann@users.noreply.github.com>
2024-02-06 04:51:52 -0300

    Add metodos put e delete

--- role-app/src/app/api/route.js
+++ role-app/src/app/api/route.js
@@ -1,29 +1,53 @@
 import fs from "fs";
 import { NextResponse } from "next/server";
-
-export function GET(request, res) {
-	const data = fs.readFileSync(
-		"C:/Users/amain/Documents/TrabCpd/role-app/src/app/bd/places.json",
-		"utf-8"
-	);
-	return new NextResponse(data);
+import all from "../bd/places.json";
+import path from "path";
+// usamos path para evitar conflitos de caminho entre diferentes SOs
+const universalPath = path.join(
+	"../role-app",
+	"src",
+	"app",
+	"bd",
+	"places.json"
+);
+export async function GET(request, res) {
+	const data = all;
+	return Response.json(data);
 }
 
 export async function POST(request) {
 	const newData = await request.json(); // assuming your request includes the updated data
-	const filePath =
-		"C:/Users/amain/Documents/TrabCpd/role-app/src/app/bd/places.json"; // Update the path to your JSON file
-
-	// Read existing data
-	const existingData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-	existingData.push(newData);
-	// Merge existing data with new data
-
+	const existingData = all.all;
+	const DataAdded = existingData.concat(newData); // Merge existing data with new data
+	all.all = DataAdded;
 	// Write back to the file
-	fs.writeFileSync(filePath, JSON.stringify(existingData));
+	//../role-app/src/app/bd/place.json
+	fs.writeFileSync(universalPath, JSON.stringify(all));
 	return new Response(JSON.stringify(newData), {
 		headers: { "Content-Type": "application/json" },
 		status: 201,
 	});
-	// Respond with a JSON object
+}
+
+export async function DELETE(request) {
+	const newData = await request.json();
+	console.log(newData);
+	const index = all.all.findIndex((place) => place.id === parseInt(newData.id));
+
+	const delPlace = all.all[index];
+	all.all.splice(index, 1);
+	fs.writeFileSync(universalPath, JSON.stringify(all));
+
+	return new Response(JSON.stringify({ text: "Tudo ok na remocao" }));
+}
+
+export async function PUT(request) {
+	const newData = await request.json();
+
+	const index = all.all.findIndex((place) => place.id === parseInt(newData.id));
+
+	const UpPlace = all.all[index];
+	all.all[index] = { id: 0, name: UpPlace.name, type: UpPlace.type };
+	fs.writeFileSync(universalPath, JSON.stringify(all));
+	return new Response(JSON.stringify({ text: "Tudo ok na att" }));
 }

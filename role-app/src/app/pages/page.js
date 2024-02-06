ee4e611dc8847c2b24aece97fc438248238f7d80
Andre-klarmann <112667579+Andre-klarmann@users.noreply.github.com>
2024-02-05 21:03:29 -0300

    Update page.js

--- role-app/src/app/pages/page.js
+++ role-app/src/app/pages/page.js
@@ -1,16 +1,65 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { showData, fetchData } from "../../../nav";
 
 export default function Home() {
 	const [searchText, setSearchText] = useState("");
-
+	const [placi, setPlaces] = useState(false);
+	let i = 0;
+	let newPlaces = [];
 	const handleSearch = async (data) => {
 		setSearchText(data.filterText);
 		const places = await showData(searchText);
-		return places;
+		setPlaces(places);
+		for (i = 0; i < placi.length; i++) {
+			newPlaces.push({ name: placi[i].name, type: placi[i].type });
+		}
+		try {
+			console.log(JSON.stringify(newPlaces));
+			const response = await fetch("http://localhost:3000/api", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(newPlaces),
+			});
+
+			if (!response.ok) {
+				throw new Error("Network response was not ok");
+			}
+
+			const result = await response.json();
+			console.log(result);
+		} catch (error) {
+			console.error("Error submitting form:", error.message);
+		}
 	};
+	if (placi) {
+		return (
+			<>
+				<>
+					<SearchForm
+						setSearchText={setSearchText}
+						handleSearch={handleSearch}
+					/>
+					<br />
+				</>
+				{placi.map((place) => {
+					return (
+						<>
+							<div>
+								<ul>
+									<li>{place.name}</li>
+									<li>{place.type}</li>
+								</ul>
+							</div>
+						</>
+					);
+				})}
+			</>
+		);
+	}
 
 	return (
 		<>
@@ -42,6 +91,7 @@ function SearchForm({ handleSearch }) {
 					required
 					{...register("filterText")}
 				/>
+
 				<button type="submit">Search</button>
 				<div className="container">{}</div>
 			</form>

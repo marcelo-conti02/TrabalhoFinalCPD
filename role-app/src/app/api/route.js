9b793b4dbb554b70d64aacfd45932dba29dd8fb1
Andre-klarmann <112667579+Andre-klarmann@users.noreply.github.com>
2024-02-06 22:30:08 -0300

    Put e delete fav_list

--- role-app/src/app/api/route.js
+++ role-app/src/app/api/route.js
@@ -1,8 +1,10 @@
 import fs from "fs";
-import { NextResponse } from "next/server";
+import Hashing from "../../../hash";
 import all from "../bd/places.json";
 import path from "path";
+import favList from "../bd/favList.json";
 // usamos path para evitar conflitos de caminho entre diferentes SOs
+
 const universalPath = path.join(
 	"../role-app",
 	"src",
@@ -10,9 +12,17 @@ const universalPath = path.join(
 	"bd",
 	"places.json"
 );
+const universalPathFav = path.join(
+	"../role-app",
+	"src",
+	"app",
+	"bd",
+	"favList.json"
+);
 export async function GET(request, res) {
 	const data = all;
-	return Response.json(data);
+	data.all = data.all.filter((obj) => obj.name.trim() !== "");
+	return Response.json({ data, favList });
 }
 
 export async function POST(request) {
@@ -31,12 +41,17 @@ export async function POST(request) {
 
 export async function DELETE(request) {
 	const newData = await request.json();
-	console.log(newData);
-	const index = all.all.findIndex((place) => place.id === parseInt(newData.id));
 
-	const delPlace = all.all[index];
-	all.all.splice(index, 1);
+	const index = favList.fav_list.findIndex(
+		(place) => place.id === parseInt(newData.id)
+	);
+
+	const delPlace = favList.fav_list[index];
+	favList.fav_list.splice(index, 1);
+	all.all[index] = delPlace;
+
 	fs.writeFileSync(universalPath, JSON.stringify(all));
+	fs.writeFileSync(universalPathFav, JSON.stringify(favList));
 
 	return new Response(JSON.stringify({ text: "Tudo ok na remocao" }));
 }
@@ -47,7 +62,9 @@ export async function PUT(request) {
 	const index = all.all.findIndex((place) => place.id === parseInt(newData.id));
 
 	const UpPlace = all.all[index];
-	all.all[index] = { id: 0, name: UpPlace.name, type: UpPlace.type };
+	all.all[index].fav = true;
+	favList.fav_list.push(UpPlace);
 	fs.writeFileSync(universalPath, JSON.stringify(all));
+	fs.writeFileSync(universalPathFav, JSON.stringify(favList));
 	return new Response(JSON.stringify({ text: "Tudo ok na att" }));
 }
